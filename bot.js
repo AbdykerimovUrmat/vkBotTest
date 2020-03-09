bffee5ea19edccfdeb19aac39dfe1b40d72766d2
Abdykerimov Urmat <55247611+AbdykerimovUrmat@users.noreply.github.com>
2020-03-09 10:53:53 +0300

    sm sh!t

--- bot.js
+++ bot.js
@@ -10,12 +10,116 @@ const { Botact } = require("botact"),
   });
 let users_info = new Map(); // users info
 
-const quiz = require("quiz.json");
+let quiz = {
+
+  
+
+  "movies" : [
+    {
+      "question": "Самый первый фильм, в котором использовали компьютерую графику - ...", 
+      "answer": "star wars"
+    }, 
+    {
+      "question": "Сколько Тимур получит по матану?", 
+      "answer": "3"
+    },
+    {
+      "question": "Будете у нас на ... , милости просим! Что вставить вместо точек?", 
+      "answer": "колыме"
+    },
+    {
+      "question": "В каком фильме?", 
+      "answer": "колыме"
+    }
+  ], 
+
+  
+
+  "music" : [
+    {
+      "question": "Милая ... , милая ...", 
+      "answer": "пони"
+    }, 
+    {
+      "question": "лучший рэпер 2019?", 
+      "answer": "mcboroda"
+    },
+    {
+      "question": "", 
+      "answer": ""
+    },
+    {
+      "question": "Чей день рождения самый важный на свете? (М... д.....и", 
+      "answer": "моей девушки"
+    },
+    {
+      "question": "Чей день рождения самый важный на свете? (М... д.....и", 
+      "answer": "моей девушки"
+    }
+  ], 
+
+  
+  
+  "games" : [
+    {
+      "question": "Как зовут брата Марио?", 
+      "answer": "луиджи"
+    }, 
+    {
+      "question": "Самая продаваемая игра после Тетриса - ...", 
+      "answer": "minecraft"
+    },
+    {
+      "question": "Полное имя главного героя GTA San Andreas - C... J.......", 
+      "answer": "carl johnson"
+    },
+    {
+      "question": "Сколько в тетрисе различных блоков?", 
+      "answer": "7"
+    },
+    {
+      "question": "Самая первая игра в мире - s.... w...", 
+      "answer": "space wars"
+    },
+    {
+      "question": "", 
+      "answer": "carl johnson"
+    },
+    {
+      "question": "Полное имя главного героя GTA San Andreas - C... J.......", 
+      "answer": "carl johnson"
+    },
+    {
+      "question": "Полное имя главного героя GTA San Andreas - C... J.......", 
+      "answer": "carl johnson"
+    },
+    {
+      "question": "Полное имя главного героя GTA San Andreas - C... J.......", 
+      "answer": "carl johnson"
+    }
+  ],
+
+
+  "localHumour" : [
+    {
+      "question": "Кто настоящий гений?", 
+      "answer": "саня"
+    }, 
+    {
+      "question": "А я .....!", 
+      "answer": "говорил"
+    },
+    {
+      "question": "", 
+      "answer": "carl johnson"
+    }
+  ]
+};
 
 let intToGame = {
   "1": "movies", 
   "2": "music", 
-  "3": "art"
+  "3": "games"
 };
 
 function getRandInd(user) {
@@ -35,16 +139,17 @@ function getRandInd(user) {
   return rand_ind;
 }
 
-function wait() {
-  setTimeout(() => {}, 10000);
-}
+
 function checkAnswer(msg, ans) {
-  return ans.toLowerCase() == msg.toLowerCase();;
+  console.log("Cheking answer")
+  console.log(ans.toLowerCase());
+  console.log(msg);
+  return (ans.toLowerCase() == msg.toLowerCase());
 }
 // quiz start
 bot.command('!start', (ctx) => {
   let user_id = ctx.user_id;
-  //first start
+  //first staart
   if(users_info.get(user_id) === undefined || !users_info.get(user_id).is_playing) {
     
     if(users_info.get(user_id) === undefined) {
@@ -58,7 +163,7 @@ bot.command('!start', (ctx) => {
         quiz_data: {
           //gotta set some size
           "movies": new Array(quiz.movies.length), 
-          "art": new Array(quiz.art.length),
+          "games": new Array(quiz.games.length),
           "music": new Array(quiz.music.length)
         }
       }); 
@@ -66,10 +171,10 @@ bot.command('!start', (ctx) => {
     
     users_info.get(user_id).quiz_data["movies"].fill(0);
     users_info.get(user_id).is_playing = 1;
-    users_info.get(user_id).quiz_data["art"].fill(0);
+    users_info.get(user_id).quiz_data["games"].fill(0);
     users_info.get(user_id).quiz_data["music"].fill(0);
 
-    bot.reply(user_id, "Выберите одну из тем: \n1)кино\n2)музыка\n3)искусство\nВведите число от 1 до 3");
+    bot.reply(user_id, "Выберите одну из тем: \n1)кино\n2)музыка\n3)игры\nВведите число от 1 до 3");
 
   } else if(users_info.get(user_id).is_playing) {
     console.log("ALREADY EXISTS")
@@ -113,10 +218,7 @@ bot.on((ctx) => {
         user.cur_ind = getRandInd(user);
         //translate to ru//
         bot.reply(user_id, "Вы выбрали тему: " + intToGame[msg]);
-
-        wait();
         bot.reply(user_id, "Если трудно ответить на вопрос, напиши 'сдаюсь'");        
-        wait();
         bot.reply(user_id, "Вот первый вопрос:");
 
       } else {
@@ -128,12 +230,12 @@ bot.on((ctx) => {
     //maybe answering
     let quesion = quiz[user.cur_game][user.cur_ind];
     let question_data = quesion["question"], question_answer = quesion["answer"];
-    wait();
     console.log(msg.toLowerCase());
+
     if(user.is_answering) {
+      console.log("User is answering");
       if(msg.toLowerCase() == "сдаюсь") {
         bot.reply(user_id, "Эх ты, бубалеха, верный ответ: " + question_answer);
-        wait();
         user.quiz_data[user.cur_game][user.cur_ind] = 1;
         let rand_ind = getRandInd(user);
         user.cur_ind = rand_ind;
@@ -151,12 +253,11 @@ bot.on((ctx) => {
         bot.reply(user_id, "Вот следующий вопрос:");
         quesion = quiz[user.cur_game][user.cur_ind];
         question_data = quesion["question"], question_answer = quesion["answer"];
-        wait();
-      } else if(checkAnswer(user, msg, question_data, question_answer)) {
+      } else if(checkAnswer(msg, question_answer)) {
         //mark question as used
+        console.log("Answer is ok");
         user.quiz_data[user.cur_game][user.cur_ind] = 1;
         bot.reply(user_id, "Умничка, верно!")
-        wait();
         let rand_ind = getRandInd(user);
         user.cur_ind = rand_ind;
         if(rand_ind == -1) {
@@ -173,11 +274,13 @@ bot.on((ctx) => {
         quesion = quiz[user.cur_game][user.cur_ind];
         question_data = quesion["question"], question_answer = quesion["answer"];
         bot.reply(user_id, "Вот следующий вопрос:");
-        wait();
+        
       } else {
+        console.log("Answer is bad");
         bot.reply(user_id, "бубалеха, ответ неверный, подуймай получше!");
         return;
       }
+      console.log("BAD");
     }
     //ask question
     bot.reply(user_id, question_data);
